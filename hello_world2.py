a9a7ae9319f344226b5ec8f02a345bfe8708ba56
xml34 <eafitense@gmail.com>
2018-11-24 22:12:34 -0500

    arreglando extensiones csv

--- hello_world2.py
+++ hello_world2.py
@@ -5,11 +5,11 @@ Created today bby
 
 from listaDiccionarios import *
 import pandas as pd
-
-word = input('introduce la palabra entre comillas: ')
+import sys
 from mpi4py import MPI
 
-def top10(word):
+#word = input('introduce la palabra entre comillas: ')
+def top10():
 	comm = MPI.COMM_WORLD
 	print ('My rank is', comm.rank)
 	lista1=[]
@@ -19,24 +19,24 @@ def top10(word):
 	    #print ('Doing the task of rank 1')
 	    art1=pd.read_csv('../../../opt/datasets/articles1.csv',header=0)
 	    listaDiccionarios(art1)
-	    lista1=listaDiccionarios.searchTop10(word)
+	    lista1=listaDiccionarios.searchTop10(sys.argv[1])
 
 	if comm.rank == 2:
 	    #print ('Haciendo la tarea del nodo 2')
 	    art2=pd.read_csv('../../../opt/datasets/articles2.csv',header=0)
 	    listaDiccionarios(art2)
-	    lista2=listaDiccionarios.searchTop10(word)
+	    lista2=listaDiccionarios.searchTop10(sys.argv[1])
 
 
 	if comm.rank == 3:
 	    #print ('faisant noeud de devoirs 3')
 	    art3=pd.read_csv('../../../opt/datasets/articles3.csv',header=0)
 	    listaDiccionarios(art3)
-	    lista3=listaDiccionarios.searchTop10(word)
+	    lista3=listaDiccionarios.searchTop10(sys.argv[1])
 
 	return lista1,lista2,lista3
 
 
-l1,l2,l3=top10(word)
+l1,l2,l3=top10()
 print(l1)
 
