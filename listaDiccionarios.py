c9af633f89f7f5c2bbda119a499d9f327683c1d9
xml34 <eafitense@gmail.com>
2018-11-24 23:55:39 -0500

    cosa

--- listaDiccionarios.py
+++ listaDiccionarios.py
@@ -11,7 +11,9 @@ class listaDiccionarios:
 		ide=csv['id']
 		for i in range(len(ide)):
 			self.diccionarios.append(diccionario(contenidos[i],ide[i],tittles[i]))
-			print(self.diccionarios[i].getCount)
+		print(self.diccionarios[2].getCount)
+		print(self.diccionarios[3].getCount)
+		print(self.diccionarios[4].getCount)
 		#return self.diccionarios
 
 	def searchTop10(self,word):
