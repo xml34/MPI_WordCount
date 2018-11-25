0be6e5749d1d1932d742f172128d1a5f205c7615
xml34 <eafitense@gmail.com>
2018-11-24 23:11:18 -0500

    cosa

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # MPI_WordCount
+
+mpiexec -f hosts_mpi -n 4 /opt/anaconda3/bin/python helloWorld_MPI.py
--- hello_world2.py
+++ hello_world2.py
@@ -5,12 +5,12 @@ Created today bby
 
 from listaDiccionarios import *
 import pandas as pd
+import numpy as np
 import sys
 from mpi4py import MPI
 
 #word = input('introduce la palabra entre comillas: ')
 def top10():
-	print("cosaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
 	print (sys.argv[1])
 	lista1=[]
 	lista2=[]
@@ -19,31 +19,31 @@ def top10():
 	    #print ('Doing the task of rank 1')
 	    art1=pd.read_csv('../../../opt/datasets/articles1.csv',header=0)
 	    lD=listaDiccionarios(art1)
-	    lista1=lD.searchTop10("house")
+	    lista1=lD.searchTop10(sys.argv[1]+"")
 
 	if comm.rank == 2:
 	    #print ('Haciendo la tarea del nodo 2')
 	    art2=pd.read_csv('../../../opt/datasets/articles2.csv',header=0)
 	    lD=listaDiccionarios(art2)
-	    lista2=lD.searchTop10("house")
+	    lista2=lD.searchTop10(sys.argv[1]+"")
 
 
 	if comm.rank == 3:
 	    #print ('faisant noeud de devoirs 3')
 	    art3=pd.read_csv('../../../opt/datasets/articles3.csv',header=0)
 	    lD=listaDiccionarios(art3)
-	    lista3=lD.searchTop10("house")
+	    lista3=lD.searchTop10(sys.argv[1]+"")
 
 	return lista1,lista2,lista3
 
 comm = MPI.COMM_WORLD
 l1,l2,l3=top10()
 if comm.rank == 1:
-	print("lista 1: ",l1)
+	print("csv 1: ",np.array(l1))
 
 if comm.rank == 2:
-	print("lista 1: ",l2)
+	print("csv 2: ",np.array(l2))
 
 if comm.rank == 3:
-	print("lista 1: ",l3)
+	print("csv 3: ",np.array(l3))
 
