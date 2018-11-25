75dbff0c5d37aee95234ecb9a8e39dc1c107158b
xml34 <eafitense@gmail.com>
2018-11-24 22:42:36 -0500

    cosa

--- hello_world2.py
+++ hello_world2.py
@@ -20,20 +20,20 @@ def top10():
 	    #print ('Doing the task of rank 1')
 	    art1=pd.read_csv('../../../opt/datasets/articles1.csv',header=0)
 	    listaDiccionarios(art1)
-	    lista1=listaDiccionarios.searchTop10(sys.argv[1])
+	    lista1=listaDiccionarios.searchTop10(sys.argv[1]+"")
 
 	if comm.rank == 2:
 	    #print ('Haciendo la tarea del nodo 2')
 	    art2=pd.read_csv('../../../opt/datasets/articles2.csv',header=0)
 	    listaDiccionarios(art2)
-	    lista2=listaDiccionarios.searchTop10(sys.argv[1])
+	    lista2=listaDiccionarios.searchTop10(sys.argv[1]+"")
 
 
 	if comm.rank == 3:
 	    #print ('faisant noeud de devoirs 3')
 	    art3=pd.read_csv('../../../opt/datasets/articles3.csv',header=0)
 	    listaDiccionarios(art3)
-	    lista3=listaDiccionarios.searchTop10(sys.argv[1])
+	    lista3=listaDiccionarios.searchTop10(sys.argv[1]+"")
 
 	return lista1,lista2,lista3
 
