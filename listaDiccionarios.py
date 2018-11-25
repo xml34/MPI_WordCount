962ba624a59b0d134b6922c549051bc21517f6a5
xml34 <eafitense@gmail.com>
2018-11-25 01:22:50 -0500

    cosa

--- listaDiccionarios.py
+++ listaDiccionarios.py
@@ -5,19 +5,18 @@ class listaDiccionarios:
 	diccionarios=[]
 
 
-	def __init__(self,csv,word):
+	def __init__(self,csv):
 		contenidos=csv['content']
 		tittles=csv['title']
 		ide=csv['id']
 		for i in range(len(ide)):
-			self.diccionarios.append(diccionario(contenidos[i],ide[i],tittles[i],word))
-
+			self.diccionarios.append(diccionario(contenidos[i],ide[i],tittles[i]))
 		#return self.diccionarios
 
 	def searchTop10(self):
 		diccionarios=[]
 		for dic in self.diccionarios:
-			diccionarios.append([dic.getId(),dic.getTittle(),dic.getCount(word)])
+			diccionarios.append([dic.getId(),dic.getTittle(),dic.getCount()])
 		diccionarios.sort(key=lambda x:x[2], reverse=True)
 		return diccionarios[:10]
 
